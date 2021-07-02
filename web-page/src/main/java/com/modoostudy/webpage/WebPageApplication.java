a3e23bdee461671dd7acbe62154133b3aa2b8164
hoon25 <cchoon95@hanmail.net>
2021-07-02 16:40:42 +0900

    Add web-page

---
+++ web-page/src/main/java/com/modoostudy/webpage/WebPageApplication.java
@@ -0,0 +1,13 @@
+package com.modoostudy.webpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebPageApplication.class, args);
+	}
+
+}
