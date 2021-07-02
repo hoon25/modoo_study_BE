ad4c726312ef3b432fb66cbe1284bfa31ff8d8dc
hoon25 <cchoon95@hanmail.net>
2021-07-02 17:07:42 +0900

    Update web-page Function

--- web-page/src/main/java/com/modoostudy/webpage/controller/webPageController.java
+++ web-page/src/main/java/com/modoostudy/webpage/controller/webPageController.java
@@ -1,2 +1,15 @@
-package com.modoostudy.webpage.controller;public class webPageController {
+package com.modoostudy.webpage.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class webPageController {
+
+    @GetMapping("/")
+    public void webPage(){
+    }
+
+
 }
